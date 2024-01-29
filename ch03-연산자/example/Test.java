d05aaa5f6fccf067bd365377222de7a8ff4e1ee7
inwoo <jiw413@naver.com>
2024-01-29 17:27:51 +0900

    자바 2, 3장 내용 정리 추가

---
+++ ch03-연산자/example/Test.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arrayList = {1, 2, 3};
+
+        System.out.println("ArrayList: " + arrayList);
+
+    }
+}
