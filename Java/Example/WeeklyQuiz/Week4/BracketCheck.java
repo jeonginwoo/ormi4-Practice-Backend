c38e7e972195f571fc5cfb1d769ee76217a9ce42
inwoo <jiw413@naver.com>
2024-02-19 09:41:02 +0900

    24-02-19 Weekly Quiz 주 알고리즘 함수로 변환

--- Java/Example/WeeklyQuiz/Week4/BracketCheck.java
+++ Java/Example/WeeklyQuiz/Week4/BracketCheck.java
@@ -6,30 +6,31 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class BracketCheck {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static boolean isMatchingBracket(String brackets) {
         Stack<Character> bracket = new Stack<>();
-
-        String brackets = br.readLine();
-        boolean isMatchingBracket = false;
         for (int i = 0; i < brackets.length(); i++) {
             if (brackets.charAt(i) == '(') {
                 bracket.push(brackets.charAt(i));
             } else {
                 if (bracket.isEmpty()) {
                     bracket.push(brackets.charAt(i));
-                    break;
+                    return false;
                 } else {
                     bracket.pop();
                 }
             }
-            System.out.println(bracket);
         }
 
         if (bracket.isEmpty()) {
-            isMatchingBracket = true;
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        System.out.println(isMatchingBracket);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(isMatchingBracket(br.readLine()));
     }
 }
