2de62992c54ee03d0be606d6b2360ebe4efdbd0d
inwoo <jiw413@naver.com>
2024-02-28 09:34:37 +0900

    24-02-27 spring boot, postgreSQL 연동해보기

---
+++ SpringBoot/blog/src/main/java/com/example/blog/dto/ArticleResponse.java
@@ -0,0 +1,21 @@
+package com.example.blog.dto;
+
+import com.example.blog.domain.Article;
+
+public class ArticleResponse {
+    private String title;
+    private String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ SpringBoot/blog/src/test/java/com/example/blog/controller/BlogControllerTest.java
@@ -0,0 +1,74 @@
+package com.example.blog.controller;
+
+import com.example.blog.domain.Article;
+import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.repository.BlogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        blogRepository.deleteAll();
+    }
+
+    @DisplayName("블로그 글 추가 성공")
+    @Test
+    public void addAriticle() throws Exception {
+        // given
+        String url = "/api/articles";
+        String title = "title";
+        String content = "contents";
+        AddArticleRequest request = new AddArticleRequest(title, content);
+
+        // 객체를 JSON으로 직렬화
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        // when : API 요청
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isCreated());
+
+        List<Article> articleList = blogRepository.findAll();
+
+        assertThat(articleList.size()).isEqualTo(1);
+        assertThat(articleList.get(0).getTitle()).isEqualTo(title);
+        assertThat(articleList.get(0).getContent()).isEqualTo(content);
+    }
+}
