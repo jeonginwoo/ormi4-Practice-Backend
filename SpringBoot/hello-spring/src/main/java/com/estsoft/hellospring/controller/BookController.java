30a2ea472081166d6472a1497d24c351db48bd78
inwoo <jiw413@naver.com>
2024-03-05 15:32:28 +0900

    spring boot 동적 페이지 구현

--- SpringBoot/hello-spring/src/main/java/com/estsoft/hellospring/controller/BookController.java
+++ SpringBoot/hello-spring/src/main/java/com/estsoft/hellospring/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-@Bean
+
 @Slf4j
 @Controller
 public class BookController {
