09e1a46d5ebfd9db322bacbe195f3b3b31c5a2b6
inwoo <jiw413@naver.com>
2024-03-07 09:41:11 +0900

    실습 초기화

---
+++ JPA/spring-db-test/src/main/java/org/example/SpringDbTestApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbTestApplication.class, args);
+    }
+}
---
+++ JPA/spring-db-test/src/main/java/org/example/repository/InstructorJdbcRepository.java
@@ -0,0 +1,13 @@
+package org.example.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InstructorJdbcRepository {
+    // TODO
+    //   1. 강사 등록
+    //   2. 모든 강사 조회
+    //   3. 이름이 XXX 인 강사 조회
+    //   4. 강사의 ID가 1번인 강사 조회
+    //   5. instructor name or desc 컬럼정보 수정하기.
+}
