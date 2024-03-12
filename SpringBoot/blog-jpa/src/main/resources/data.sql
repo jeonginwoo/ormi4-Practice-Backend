12678c2ef73c6c37f39d96a95f5ff9b9fbbd566e
inwoo <jiw413@naver.com>
2024-03-12 16:23:28 +0900

    blog jpa로 로직 구성 완료

---
+++ SpringBoot/blog-jpa/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO article (title, content) VALUES ('블로그 제목', '블로그 내용');
+INSERT INTO article (title, content) VALUES ('블로그 제목2', '블로그 내용2');
+INSERT INTO article (title, content) VALUES ('블로그 제목3', '블로그 내용3');
+
